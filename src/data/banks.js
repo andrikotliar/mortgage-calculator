7c88bbbf0bddb8b74e3baa30bbd1706c7b5bfd25
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-20 23:46:06 +0300

    Update banks data

--- src/data/banks.js
+++ src/data/banks.js
@@ -1,11 +1,11 @@
-const banks = [
+export const banks = [
   {
     "id": 1,
     "title": "Paris Bank",
     "interestRate": 25,
     "maxLoan": 300000,
     "minDownPayment": 20,
-    "loanTerm": 2
+    "loanTerm": 15
   },
   {
     "id": 2,
@@ -13,14 +13,14 @@ const banks = [
     "interestRate": 10,
     "maxLoan": 500000,
     "minDownPayment": 10,
-    "loanTerm": 1
+    "loanTerm": 5
   },
   {
-    "id": 2,
+    "id": 3,
     "title": "London Bank",
-    "interestRate": 10,
+    "interestRate": 15,
     "maxLoan": 150000,
     "minDownPayment": 30,
-    "loanTerm": 4
+    "loanTerm": 10
   }
 ]
