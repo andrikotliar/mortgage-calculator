c535674879352ae0bbf1ba1f7e94d3751133ede5
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-20 23:46:42 +0300

    Add bank params

---
+++ src/data/bankParams.js
@@ -0,0 +1,18 @@
+export const bankParams = [
+  {
+    title: "Interest Rate",
+    value: "interestRate"
+  },
+  {
+    title: "Maximum Loan",
+    value: "maxLoan"
+  },
+  {
+    title: "Minimum Down Payment",
+    value: "minDownPayment"
+  },
+  {
+    title: "Loan Term",
+    value: "loanTerm"
+  }
+]
