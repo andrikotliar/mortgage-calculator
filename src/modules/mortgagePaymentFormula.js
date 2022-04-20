4acdffea90d7c2fad4566366148cd1617f36affc
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-20 23:46:32 +0300

    Update monthlyPaymentFormula

--- src/modules/mortgagePaymentFormula.js
+++
@@ -1,8 +0,0 @@
-export const mortgagePaymentFormula = (amountBorrowed, interestRate, monthlyPaymentNum) => {
-
-  const rTo12 = interestRate / 12;
-  const formulaPart = Math.pow(1 + rTo12, monthlyPaymentNum);
-  const monthlyPayment = (amountBorrowed * (rTo12 * formulaPart)) / (formulaPart - 1);
-
-  return monthlyPayment;
-}
